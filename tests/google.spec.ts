d86cc32b4051dd81d92a849b4f64d094a3e18ae0
Joao Paulo Dias <jpdias1997@hotmail.com>
2024-12-13 18:33:27 -0300

    Remove scenarios

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,27 +1,29 @@
-import { expect, test } from "@playwright/test";
-import { ResultsPage } from "../pages/results.page";
-import { SearchPage } from "../pages/search.page";
+import { expect, test } from "@playwright/test"
+import { ResultsPage } from "../pages/results.page"
+import { SearchPage } from "../pages/search.page"
 test.describe("Google Search", () => {
-  test("Valid Search", async ({ page }) => {
-    const query = "Software Testing";
-    const expectedResult = "What Is Software Testing? | IBM";
-    const searchPage = new SearchPage(page);
-    const resultPage = new ResultsPage(page);
-    await searchPage.goto();
-    await searchPage.searchFor(query);
-    await expect(resultPage.results).toContainText([expectedResult]);
-  });
+  let searchPage: SearchPage
+  let resultPage: ResultsPage
+  const query = "Software Testing"
+  const expectedResult = "What Is Software Testing? | IBM"
+  
+  test.beforeEach(async ({ page }) => {
+    searchPage = new SearchPage(page)
+    resultPage = new ResultsPage(page)
+  })
 
-  test("Search Result Navigation", async ({ page }) => {
-    const query = "Software Testing";
-    const expectedResult = "What Is Software Testing? | IBM";
-    const searchPage = new SearchPage(page);
-    const resultPage = new ResultsPage(page);
-    await searchPage.goto();
-    await searchPage.searchFor(query);
-    await resultPage.clickOn(expectedResult);
+  test("Valid Search", async () => {
+    await searchPage.goto()
+    await searchPage.searchFor(query)
+    await expect(resultPage.results).toContainText([expectedResult])
+  })
+
+  test("Search Result Navigation", async () => {
+    await searchPage.goto()
+    await searchPage.searchFor(query)
+    await resultPage.clickOn(expectedResult)
     await expect(resultPage.page).toHaveURL(
       "https://www.ibm.com/topics/software-testing"
-    );
-  });
-});
+    )
+  })
+})
